3e4df9fe37ef4317827b00321365bd70f1dce9b4
Martin Valchev <mv1u18@soton.ac.uk>
2022-01-15 02:38:45 +0200

    base64 compression

--- packages/tldraw/src/state/data/filesystem.ts
+++ packages/tldraw/src/state/data/filesystem.ts
@@ -4,7 +4,7 @@ import type { FileSystemHandle } from './browser-fs-access'
 import { get as getFromIdb, set as setToIdb } from 'idb-keyval'
 import { IMAGE_EXTENSIONS, VIDEO_EXTENSIONS } from '~constants'
 
-import { compressToUTF16, decompressFromUTF16 } from 'lz-string'
+import { compressToBase64, decompressFromBase64 } from 'lz-string'
 
 const options = { mode: 'readwrite' as const }
 
@@ -36,7 +36,7 @@ export async function saveToFileSystem(document: TDDocument, fileHandle: FileSys
   }
 
   // Serialize to JSON
-  const json = compressToUTF16(JSON.stringify(file))
+  const json = compressToBase64(JSON.stringify(file))
 
   // Create blob
   const blob = new Blob([json], {
@@ -76,7 +76,7 @@ export function docToString(document: TDDocument) {
     assets: {},
   }
 
-  return compressToUTF16(JSON.stringify(file))
+  return compressToBase64(JSON.stringify(file))
 }
 
 export async function openFromFileSystem(): Promise<null | {
@@ -107,7 +107,7 @@ export async function openFromFileSystem(): Promise<null | {
   })
 
   // Parse
-  const file: TDFile = JSON.parse(decompressFromUTF16(json) ?? '')
+  const file: TDFile = JSON.parse(decompressFromBase64(json) ?? '')
 
   const fileHandle = blob.handle ?? null
 
@@ -120,7 +120,7 @@ export async function openFromFileSystem(): Promise<null | {
 }
 
 export function openFromString(docAsString: string): TDFile {
-  return JSON.parse(decompressFromUTF16(docAsString) ?? '')
+  return JSON.parse(decompressFromBase64(docAsString) ?? '')
 }
 
 export async function openAssetFromFileSystem() {
